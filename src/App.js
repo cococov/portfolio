f8837d68bf40ce3919fa50fc2679f6cae92d8fe6
cococov <juank.yea@hotmail.com>
2019-09-11 04:09:12 -0300

    curriculum cargado y varias dependencias instaladas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import logo from './static/img/workinprogressheader.png';
+import Button from '@material-ui/core/Button';
+import {cv} from './config.json'
+import './static/css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { 
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+
+  openCv = () => {
+    window.open(cv);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Button variant="contained" size="small" onClick={this.openCv}>
+            curriculum
+          </Button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
