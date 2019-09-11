2433b46fe37603c48e6b879c69085d6ba8cb345c
cococov <juank.yea@hotmail.com>
2019-09-11 01:12:40 -0300

    Updates

---
+++ precache-manifest.90c2d43453294d5728637c76fa2c3101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ad861ebb7e8ec8f98d7c233f4e3829",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6a909522ecb7bc52a491",
+    "url": "/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2cc8a9b70acebea7a750",
+    "url": "/portfolio/static/js/2.fadd7498.chunk.js"
+  },
+  {
+    "revision": "6a909522ecb7bc52a491",
+    "url": "/portfolio/static/js/main.4ae70f40.chunk.js"
+  },
+  {
+    "revision": "255cf37ac81f61801b31",
+    "url": "/portfolio/static/js/runtime~main.36963274.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
