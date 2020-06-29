37582ba4b040a69ff68710f23eda168b5e8581dc
cococov <juank.yea@hotmail.com>
2020-06-28 22:56:40 -0400

    scroll problem solved + scroll problem when the content changes, solved

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, memo, useMemo } from 'react';
+import React, { useLayoutEffect, useContext, memo, useMemo } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import ReactMarkdown from 'markdown-to-jsx';
 import clsx from 'clsx';
@@ -14,12 +14,18 @@ const Container = withStyles(ContentStyles)(({ classes }) => {
   const { selectedContent } = useContext(ContentContext);
   const { isMobil, width } = useContext(AppContext);
 
+  useLayoutEffect(() => {
+    let _this = document.getElementById('content');
+    _this.scrollTo(0, 0);
+  }, [selectedContent])
+
   return useMemo(() => (
     <div
       style={!isMobil ? { width } : {}}
       className={classes.container}
     >
       <div
+        id="content"
         className={clsx('Content', classes.content)}
       >
         <ReactMarkdown
